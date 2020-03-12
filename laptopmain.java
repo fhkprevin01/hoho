06bbe85ace29b9d4e92387a3854635039ac8c238
fhkprevin01 <fhkprevin01@gmail.com>
2020-03-12 23:11:47 +0800

    laptopmain

---
+++ laptopmain.java
@@ -0,0 +1,7 @@
+public class laptopmain{
+    public static void main(String[] args) {
+        laptop laptop1 = new laptop();
+        laptop1.setlaptop("15.6","black","16G","2.4kg","asus");
+        laptop1.getlaptop();
+    }
+}

23abb85be801f9f7245776d48aab024e52dfb015
fhkprevin01 <fhkprevin01@gmail.com>
2020-03-12 23:12:52 +0800

    studentmain

---
+++ studentmain.java
@@ -0,0 +1,7 @@
+public class studentmain{
+    public static void main(String[] args) {
+        student student1 = new student();
+        student1.setstudent("yoyo","yellow","blue","156","red");
+        student1.getstudent();
+    }
+}
